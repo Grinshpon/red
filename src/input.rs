a6ebcbe58a16ca9703a8cd70cd932bfa649b5487
grinshpon <grinshpondaniel@protonmail.com>
2019-08-08 15:02:43 -0400

    setting up skeletons

--- src/input.rs
+++ src/input.rs
@@ -5,7 +5,12 @@ use std::io::{Read, Write};
 use std::thread;
 use std::time::Duration;
 
-fn input_handler(scr: &mut dyn Write, ) {
+pub struct Input; //will become command
+pub type Done = Vec<Input>; //will become "Done" stack
+pub type Undone = Vec<Input>; //will become "Undone" stack
+pub type Comm = Vec<Input>; //will become command mode input stack
+
+pub fn input_handler(scr: &mut dyn Write, ) {
     let mut stdin = async_stdin().bytes();
     let mut last_input = String::new();
 
