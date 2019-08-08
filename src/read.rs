a54d3258dc32a5ae38fc45a66bad8564d69b1099
grinshpon <grinshpondaniel@protonmail.com>
2019-08-08 12:07:06 -0400

    swp logic

--- src/read.rs
+++ src/read.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::fs::File;
 use std::path::Path;
 use std::io;
@@ -15,8 +16,16 @@ pub fn open_file(mfp: Option<String>) -> io::Result<RFile> {
       let fp = Path::new(&fps);
       if fp.is_file() {
         let temp_main = File::open(fp)?;
-        let temp_swap = File::create(Path::new( &format!("{}{}",fps,(".swp")) ))?;
-        Ok(RFile{name: fps, main: Some(temp_main), swap: temp_swap})
+        let swap_fps = format!("{}{}",fps,(".swp"));
+        let swap_fp = Path::new(&swap_fps);
+        if swap_fp.exists() {
+          Err(Error::new(ErrorKind::Other, "Swap file already exists")) //TODO: add existing swap handling
+        }
+        else {
+          let temp_swap = File::create(swap_fp)?;
+          fs::copy(fp, swap_fp)?;
+          Ok(RFile{name: fps, main: Some(temp_main), swap: temp_swap})
+        }
       }
       else if fp.exists() { // means the entity exists but is not a file
         Err(Error::new(ErrorKind::Other, "Cannot open directory"))
