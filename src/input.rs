b5e80a1417a2674d3dc84b129cef65e1badb6497
grinshpon <grinshpondaniel@protonmail.com>
2019-08-08 23:59:17 -0400

    bin in cargo.toml

--- src/input.rs
+++ src/input.rs
@@ -11,9 +11,24 @@ pub struct Input { //will become command
   ch: Char,
   position: Pos,
 }
-pub type Done = Vec<Input>; //will become "Done" stack
-pub type Undone = Vec<Input>; //will become "Undone" stack
-pub type Comm = Vec<Input>; //will become command mode input stack
+pub type Action = Vec<Input>;
+pub type Done = Vec<Action>; //will become "Done" stack
+pub type Undone = Vec<Action>; //will become "Undone" stack
+pub type Comm = Vec<Action>; //will become command mode input stack
+
+/*
+Basicallly how this works:
+  When in input mode, any characters you press are saved in an action: an
+  input chain. After a brief period of inactivity or if `esc` is pressed,
+  the action chain is considered closed/complete. Then the action is pushed
+  onto the "done" stack.
+
+  When undoing an action, it's effects are reversed, and the action is popped
+  from the "done" stack and pushed onto the "undone" stack.
+
+  When redoing an action, it's effects are applied, and the action is popped
+  from the "undone" stack and pushed back onto the "done" stack.
+*/
 
 pub fn input_handler(scr: &mut dyn Write, ) {
     let mut stdin = async_stdin().bytes();
